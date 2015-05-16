77bf9867b6cb46f91b49df25e1399266522617ec
swcho <sungwoo.cho.dev@gmail.com>
2015-05-16 13:16:00 +0900

    ex

---
+++ sqls/1/ins_tea.sql
@@ -0,0 +1,37 @@
+-------------------------------------------------------------------------
+--  FILENAME : sample_insert_team_t.sql  
+-------------------------------------------------------------------------
+--  DESCRIPTION : 
+--    K-리그 : 팀정보 테이블 (TEAM_T)  
+-------------------------------------------------------------------------
+
+INSERT INTO team_t VALUES ('K10','대전','시티즌','TAEJON CITIZEN','1997','E02','301','030',
+'대전광역시 중구 부사동 162-1 한밭운동장 내 ','42','252-2002','221-0669','http://www.tcfc.co.kr','');                                                
+                                                                               
+INSERT INTO team_t VALUES ('K06','부산','아이콘스','BUSAN ICONS','1983','C02','57 ','050',
+'부산시 동래구 수안동 1-11번지 대신증권 B/D 9층','51','555-7101','555-7103','http://www.busanicons/co.kr','');                                        
+                                                                                
+INSERT INTO team_t VALUES ('K04','부천','SK','BUCHEON SK','1982','B01','110','728',
+'서울 종로구 서린동 99번지 SK 빌딩 3층','2','2121-5271','2121-5276','http://www.skfc.com','');                                                               
+
+INSERT INTO team_t VALUES ('K08','성남','일화천마','SEONGNAM ILHWA','1989','B02','462',
+'130','경기도 성남시 중원구 성남동 3110번지 성남종합운동장 내','31','753-3956','753-4443','http://www.seongnamilhwafc.co.kr','');                        
+                                                                                
+INSERT INTO team_t VALUES ('K02','수원','삼성블루윙즈','SUWON SAMSUNG BLUEWINGS','1995','B04','440','220',
+'수원시 장안구 조원동 601-1 수원시체육회관 3층','31','247-2002','257-0766','http://www.fcbluewings.com','');                          
+                                                                                
+INSERT INTO team_t VALUES ('K09','안양','LG치타스','ANYANG LG ','1983','B05','138','221',
+'서울 송파구 잠실1동 10번지 종합운동장 내','2','2005-5746','2005-5802','http://www.lgfootball.com','');                                                
+                                                                                
+INSERT INTO team_t VALUES ('K01','울산','현대호랑이','ULSAN HYUNDAI HORANGI','1983','C04','682','060',
+'울산광역시 동구 전하동 1번지','52','230-6141','230-6145','http://www.horanifc.co.kr','');                                                
+                                                                                
+INSERT INTO team_t VALUES ('K07','전남','드래곤즈','CHUNNAM DRAGONS','1994','D01','544','010',
+'전남 동광양시 중동 1318-2번지 산업은행 B/D 5층','61','792-5600','792-5605','http://www.dragons.co.kr','');                                       
+                                                                                
+INSERT INTO team_t VALUES ('K05','전북','현대모터스','CHUNBUK HYUNDAI MOTORS','1994','D03','560','190',
+'전북 전주시 덕진구 덕진동 1가 1220번지','63','273-1763','273-1762','http://www.hyundai-motors.com','');                                 
+                                                                                
+INSERT INTO team_t VALUES ('K03','포항','스틸러스','POHANG STEELERS','1973','C06','790','050',
+'경북 포항시 죽도동 645-1 삼일 B/D 6층','54','282-2002','282-5955','http://www.steelers.co.kr','');                                               
+ 
